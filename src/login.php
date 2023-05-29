1598221727b2e22c7db86224c785992b52c7bdfb
Leonardo Beleffi <leonardo.beleffi@studio.unibo.it>
2023-05-29 23:29:16 +0200

    Add empty css for login

--- src/login.php
+++ src/login.php
@@ -1,35 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="UTF-8"/>
-    <meta name="application-name" content="SolveIT"/>
-    <meta name="description" content="UNIBO Web Project"/>
-    <meta name="author" content="Beleffi Leonardo, Sangiorgi Marco, Vuksan Tiziano"/>
-    <meta name="keywords" content="Solveit, Solvit, Solveet, Slovit, Solveti, Bug, Solution, Solve, Fix, StackOverflow">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <meta name="application-name" content="SolveIT" />
+    <meta name="description" content="UNIBO Web Project" />
+    <meta name="author" content="Beleffi Leonardo, Sangiorgi Marco, Vuksan Tiziano" />
+    <meta name="keywords" content="Solveit, Solvit, Solveet, Slovit, Solveti, Bug, Solution, Solve, Fix, StackOverflow" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Log In - SolveIT</title>
+    <link rel="stylesheet" type="text/css" href="./css/login.css" />
   </head>
   <body>
     <main>
       <header>
         <div>
 <!-- logo -->
-          <img src="../resources/images/logo" alt="logo"/>
+          <img src="../resources/images/logo" alt="logo" />
         </div>
       </header>
 
       <main>
         <h1>Log In</h1>
         <form>
-          <label>Username<input type="text" id="username" name="username"/></label>
-          <label>Password<input type="password" id="password" name="password"/></label>
+          <label>Username<input type="text" id="username" name="username" /></label>
+          <label>Password<input type="password" id="password" name="password" /></label>
+          <a href="./forgotPassword.php">Forgot your password?</a>
+          <a href="./forgotUsername.php">Forgot your username?</a>
+          <input type="submit" value="Log In" />
         </form>
       </main>
 
       <footer>
         <a href="./about.php">
 <!-- ALT tutorial -->
-          <img src="../resources/icons/INFO_ABOUT" alt="Informations about this site"/>
+          <img src="../resources/icons/INFO_ABOUT" alt="Informations about this site" />
         </a>
       </footer>
     </main>
