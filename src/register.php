32c01115a171dd1896ae56aad8cb82beec2cfb69
Leonardo Beleffi <leonardo.beleffi@studio.unibo.it>
2023-08-06 16:21:47 +0200

    Add fields for registration

--- src/register.php
+++ src/register.php
@@ -21,6 +21,21 @@
                 <h1>Sign Up</h1>
                 <form>
                     <div>
+                        <label>Name<input type="text" id="name" name="name" /></label>
+                    </div>
+                    <div>
+                        <label>Surname<input type="text" id="surname" name="surname" /></label>
+                    </div>
+                    <div>
+                        <label>Date of birth<input type="date" id="birth_date" name="birth_date" /></label>
+                    </div>
+                    <div>
+                        <label>Phone number<input type="number" id="phone_number" name="phone_number" /></label>
+                    </div>
+                    <div>
+                        <label>Email<input type="email" id="email" name="email" /></label>
+                    </div>
+                    <div>
                         <label>Username<input type="text" id="username" name="username" /></label>
                     </div>
                     <div>
