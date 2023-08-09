dd828724e92b85e2bf68e1fe3554cf5bacf404f3
Leonardo Beleffi <leonardo.beleffi@studio.unibo.it>
2023-08-09 17:53:33 +0200

    Fix div in new register page

--- src/register.php
+++ src/register.php
@@ -12,43 +12,32 @@
     </head>
     <body>
         <header>
-            <!-- logo -->
             <img src="/resources/icons/logo.svg" alt="logo" />
         </header>
 
         <main class="flexContainer">
             <section  id="registerSection">
-                <h1>Sign Up</h1>
+                <div>
                 <form>
+                    <h1>Sign Up</h1>
                     <div>
-                        <label>Name<input type="text" id="name" name="name" /></label>
-                    </div>
-                    <div>
-                        <label>Surname<input type="text" id="surname" name="surname" /></label>
-                    </div>
-                    <div>
-                        <label>Date of birth<input type="date" id="birth_date" name="birth_date" /></label>
-                    </div>
-                    <div>
-                        <label>Phone number<input type="number" id="phone_number" name="phone_number" /></label>
-                    </div>
-                    <div>
-                        <label>Email<input type="email" id="email" name="email" /></label>
-                    </div>
-                    <div>
-                        <label>Username<input type="text" id="username" name="username" /></label>
-                    </div>
-                    <div>
-                        <label>Password<input type="password" id="password" name="password" /></label>
-                    </div>
-                    <div>
-                        <a href="./login.php">Already an account? Sign in here!</a>
-                    </div>
-                    <div> 
+                        <div>
+                            <label>Name<input type="text" id="name" name="name" /></label>
+                            <label>Surname<input type="text" id="surname" name="surname" /></label>
+                            <label>Date of birth<input type="date" id="birth_date" name="birth_date" /></label>
+                            <label>Phone number<input type="number" id="phone_number" name="phone_number" /></label>
+                            <label>Email<input type="email" id="email" name="email" /></label>
+                            <label>Username<input type="text" id="username" name="username" /></label>
+                            <label>Password<input type="password" id="password" name="password" /></label>
+                        </div>
                         <input type="submit" value="Sign Up" />
                     </div>
                 </form>
+                <div>
             </section>
+            <footer>
+                <p>Already an accout? <a href="./login.php">Log in here!</a></p>
+            </footer>
         </main>
 
         <footer>
