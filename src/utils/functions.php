b6be44dcae25d15ab848dec3b0be9f4f65fc78d9
Leonardo Beleffi <leonardo.beleffi@studio.unibo.it>
2023-08-12 17:46:26 +0200

    Add error msg in register page

--- src/utils/functions.php
+++ src/utils/functions.php
@@ -14,6 +14,17 @@
         return getSessionVar("errorMsg");
     }
 
+    function loadErrorMsg(){
+        $msg = getErrorMsg();
+        if(empty($msg)) return;
+        setErrorMsg("");
+        echo    "<div class=\"errorDiv\">" .
+                    "<p class=\"errorP\">" .
+                        "ERROR:" . $msg .
+                    "</p>" .
+                "</div>";
+    }
+
     // ----------------- LOGIN functions
     function checkUserLogin(){
         if(!isUserLoggedIn()) {
@@ -54,13 +65,16 @@
         unset($_SESSION["main"]);
         unset($_SESSION["footer"]);
     }
+
     //CSS
     function setCSS($CSS){
         $_SESSION["CSS"] = $CSS;
     }
+
     function getCSS(){
         return getSessionVar("CSS");
     }
+
     function loadCSS(){
         if(!empty(getCSS())) : 
             foreach(getCSS() as $css):
@@ -68,13 +82,16 @@
             endforeach;
         endif;
     }
+
     //JS
     function setJS($JS){
         $_SESSION["JS"] = $JS;
     }
+
     function getJS(){
         return getSessionVar("JS");
     }
+
     function loadJS(){
         if(!empty(getJS())) : 
             foreach(getJS() as $js):
@@ -82,31 +99,39 @@
             endforeach;
         endif;
     }
+
     //Title
     function setTitle($title){
         $_SESSION["title"] = $title;
     }
+
     function getTitle(){
         return getSessionVar("title");
     }
+
     //Header
     function setHeader($header){
         $_SESSION["header"] = $header;
     }
+
     function getHeader(){
         return getSessionVar("header");
     }
+
     //Main
     function setMain($main){
         $_SESSION["main"] = $main;
     }
+
     function getMain(){
         return getSessionVar("main");
     }
+
     //Footer
     function setFooter($footer){
         $_SESSION["footer"] = $footer;
     }
+
     function getFooter(){
         return getSessionVar("footer");
     }
@@ -127,3 +152,4 @@
         exit();
     }
 ?>
+
