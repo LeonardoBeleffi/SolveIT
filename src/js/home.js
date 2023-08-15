70bf6c0f5a931eb8800a2ee42bbc3e62c93a133d
Leonardo Beleffi <leonardo.beleffi@studio.unibo.it>
2023-08-15 17:59:12 +0200

    Add js to make the bar change colour on click

---
+++ src/js/home.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let nav_bar_links;
+
+window.addEventListener("load", () => {
+    nav_bar_links = Array.from(document.querySelectorAll("#nav_bar")[0].children);
+
+    nav_bar_links.forEach(event => addEventListener("click", (event) => {
+        let e = event.target;
+        if (e.className === "selected_link")  return;
+        nav_bar_links.forEach(element => element.className = "");
+        e.className = "selected_link";
+    }));
+});
+
