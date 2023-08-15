c7e639603e72dbd964b15cf2b90c40ae4c49e50a
Leonardo Beleffi <leonardo.beleffi@studio.unibo.it>
2023-08-15 19:20:47 +0200

    Fix click not on links

--- src/js/home.js
+++ src/js/home.js
@@ -5,11 +5,14 @@ let nav_bar_links;
 window.addEventListener("load", () => {
     nav_bar_links = Array.from(document.querySelectorAll("#nav_bar")[0].children);
 
-    nav_bar_links.forEach(event => addEventListener("click", (event) => {
-        let e = event.target;
-        if (e.className === "selected_link")  return;
-        nav_bar_links.forEach(element => element.className = "");
-        e.className = "selected_link";
-    }));
-});
+    nav_bar_links.forEach(event => {
+        addEventListener("click", (event) => {
+            let e = event.target;
+            if (e.tagName !== 'A') return;
+            if (e.className === "selected_link")  return;
+            nav_bar_links.forEach(element => element.className = "");
+            e.className = "selected_link";
+        });
+    });
+}, false);
 
