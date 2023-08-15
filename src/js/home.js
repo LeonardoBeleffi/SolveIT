b5f6f560ff2e71bc05ac8d49dc83fbb6544b3580
Leonardo Beleffi <leonardo.beleffi@studio.unibo.it>
2023-08-16 00:50:54 +0200

    Add post opening and closing

--- src/js/home.js
+++ src/js/home.js
@@ -1,18 +1,50 @@
 "use strict";
 
 let nav_bar_links;
+let posts;
+let body;
+let main;
+let footer;
 
 window.addEventListener("load", () => {
     nav_bar_links = Array.from(document.querySelectorAll("#nav_bar")[0].children);
+    posts = Array.from(document.querySelectorAll("#post_list")[0].children);
+    main = document.querySelectorAll("main")[0];
+    body = document.querySelectorAll("body")[0];
+    footer = body.children[2];
 
     nav_bar_links.forEach(event => {
-        addEventListener("click", (event) => {
+        event.addEventListener("click", (event) => {
             let e = event.target;
-            if (e.tagName !== 'A') return;
             if (e.className === "selected_link")  return;
             nav_bar_links.forEach(element => element.className = "");
             e.className = "selected_link";
         });
     });
+
+    posts.forEach(post => {
+        post.addEventListener("click", (event) => {
+            let e = event.target;
+            post.children[0].style.height = "100%";
+            post.children[0].children[0].className = "";
+            post.children[0].children[1].style.display = "block";
+            posts.forEach(element => {
+                let elementPreview = element.children[0].children[0];
+                if (elementPreview !== e) element.style.display = "none";
+            });
+        }, false);
+    });
+
+    body.addEventListener("click", (event) => {
+        let e = event.target;
+        if (main.children[0].contains(e) || footer.contains(e)) return;
+        posts.forEach(element => {
+            element.style.display = "block";
+            element.children[0].children[0].className = "post_preview";
+            // element.children[0].style.height = "15vh";
+            element.children[0].children[1].style.display = "none";
+        });
+    });
+
 }, false);
 
