b24e3717ea73b7ed1fb87f80498c548e5074150b
sangiom <marco.sangiorgi24@studio.unibo.it>
2023-08-22 17:15:40 +0200

    add compatibility features
    add uppercase letter in tables names for InnoDB compatibility.

--- db/InsertData.sql
+++ db/InsertData.sql
@@ -1,101 +1,101 @@
 USE `epiz_34305586_solveit` ;
 
-select * from utente;
+select * from Utente;
 
-INSERT INTO `utente` ( `email`, `nome`, `username`) VALUES
+INSERT INTO `Utente` ( `email`, `nome`, `username`) VALUES
 ('admin@blogtw.com', 'Admin Nimda', 'admin'),
 ('sangio@blogtw.com', 'Gino Pino', 'sangio'),
 ('leo@blogtw.com', 'Cippa Lippa', 'leo'),
 ('tizz@blogtw.com', 'Chico Loco', 'tiz');
 
-INSERT INTO `credenziali` ( `password`, `idUtente`) VALUES
+INSERT INTO `Credenziali` ( `password`, `idUtente`) VALUES
 ('admin', '1'),
 ('sangio', '2'),
 ('leo', '3');
 
 
-INSERT INTO `amicizia` (`idSeguace`, `idSeguito`, `timestamp`) VALUES
+INSERT INTO `Amicizia` (`idSeguace`, `idSeguito`, `timestamp`) VALUES
 (1, 2, '2023-07-01 21:19:03'),
 (2, 1, '2023-07-03 13:40:53');
 
 
-INSERT INTO `settore` (`nomeSettore`) VALUES
+INSERT INTO `Settore` (`nomeSettore`) VALUES
 ('officina');
 
 
-INSERT INTO `post` (`idsettore`, `titolo`, `testo`, `timestamp`) VALUES
-(1, 'Il mio post su SolveIt!', 'Se vi capita questo allora fate quest\'altro e via track track tutto funziona!','2023-07-01 21:19:03');
+INSERT INTO `Post` (`idSettore`, `titolo`, `testo`, `timestamp`) VALUES
+(1, 'Il mio Post su SolveIt!', 'Se vi capita questo allora fate quest\'altro e via track track tutto funziona!','2023-07-01 21:19:03');
 
 
-INSERT INTO `pubblicazione` (`idutente`, `idpost`) VALUES
+INSERT INTO `Pubblicazione` (`idUtente`, `idPost`) VALUES
 (2, 1);
 
 
-INSERT INTO `tag` (`nome`) VALUES
+INSERT INTO `Tag` (`nome`) VALUES
 ('GUI'),
 ('Back-end');
 
 
-INSERT INTO `etichettamento` (`idpost`, `idtag`) VALUES
+INSERT INTO `Etichettamento` (`idPost`, `idTag`) VALUES
 (1, 2);
 
-INSERT INTO `commento` (`idpost`, `idutente`, `testo`, `timestamp`) VALUES
-(1, 1, 'bel post, caspita un botto utile!', '2023-07-01 21:19:03'),
+INSERT INTO `Commento` (`idPost`, `idUtente`, `testo`, `timestamp`) VALUES
+(1, 1, 'bel Post, caspita un botto utile!', '2023-07-01 21:19:03'),
 (1, 2, 'Lo sapevamo già', '2023-07-01 21:19:03');
 
-INSERT INTO `mipiace` (`idpost`, `idutente`) VALUES
+INSERT INTO `MiPiace` (`idPost`, `idUtente`) VALUES
 (1, 1),
 (1, 2);
 
-select * from post;
-select * from pubblicazione;
-select * from allegato;
-select * from post;
-select * from settore;
-select * from utente;
-select * from credenziali;
-select * from tag;
-select * from etichettamento;
-select * from commento;
-select * from mipiace;
+select * from Post;
+select * from Pubblicazione;
+select * from Allegato;
+select * from Post;
+select * from Settore;
+select * from Utente;
+select * from Credenziali;
+select * from Tag;
+select * from Etichettamento;
+select * from Commento;
+select * from MiPiace;
 
-# idPost --> idPost, autore, testo, settore, timestamp
-SELECT p.idPost, u.nome as autore, p.testo, s.nomeSettore as settore, p.timestamp
-        FROM post as p, settore as s, pubblicazione as pub, utente as u
+# idPost --> idPost, autore, testo, Settore, timestamp
+SELECT p.idPost, u.nome as autore, p.testo, s.nomeSettore as Settore, p.timestamp
+        FROM Post as p, Settore as s, Pubblicazione as pub, Utente as u
         where p.idPost = 1
         and p.idPost = pub.idPost and p.idSettore = s.idSettore and pub.idUtente = u.idUtente;
 
-# idPost --> idTag, tag
-SELECT t.idTag, nome as tag
-		FROM tag as t, etichettamento as e
+# idPost --> idTag, Tag
+SELECT t.idTag, nome as Tag
+		FROM Tag as t, Etichettamento as e
 		where e.idPost = 1 and e.idTag = t.idTag;
         
-# idPost --> idAllegato, allegato[bytes], tipo
-SELECT a.idAllegato, data as allegato, tipo
-        FROM allegato as a
+# idPost --> idAllegato, Allegato[bytes], tipo
+SELECT a.idAllegato, data as Allegato, tipo
+        FROM Allegato as a
         where a.idPost = 1;
         
 # idPost --> idUtente, username
 SELECT u.idUtente, username
-        FROM utente as u, pubblicazione as p
+        FROM Utente as u, Pubblicazione as p
         where p.idPost = 1 and p.idUtente = u.idUtente;
         
 # prefix ---> idUtente, nome, cognome, username, idSettore
 SELECT * 
-		FROM utente
+		FROM Utente
 		where username LIKE 'l%';
         
 # idUtente --> idUtente, nome, cognome, username, idSettore, inizioAmicizia
 SELECT u.idUtente, u.nome, u.cognome, u.username, u.idSettore, a.timestamp as inizioAmicizia
-        FROM utente as u, amicizia as a
+        FROM Utente as u, Amicizia as a
         where u.idUtente = a.idSeguito and a.idSeguace = 1;
 
 #idUtente --> idPubblicazione, idPost, comeCollaboratore
 SELECT idPubblicazione, idPost, collaboratore as comeCollaboratore
-        FROM pubblicazione as p
+        FROM Pubblicazione as p
         where idUtente = 2;
         
 #username,password --> idUtente, username, nome
 SELECT u.idUtente, username, nome 
-        FROM utente as u, credenziali as c 
+        FROM Utente as u, Credenziali as c 
         WHERE u.idUtente = c.idUtente and u.username = "sangio" and c.password = "admin";
