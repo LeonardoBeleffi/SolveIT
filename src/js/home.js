b4962177df96b1062b03bf02a9cb6504797f75f0
Leonardo Beleffi <leonardo.beleffi@studio.unibo.it>
2023-08-24 16:00:37 +0200

    Fix home js

--- src/js/home.js
+++ src/js/home.js
@@ -6,48 +6,64 @@ let main;
 let footer;
 
 window.addEventListener("load", () => {
-    nav_bar_links = Array.from(document.querySelectorAll("#nav_bar")[0].children);
-    posts = Array.from(document.querySelectorAll("#post_list")[0].children);
+    nav_bar_links = Array.from(document.querySelectorAll("#nav-bar")[0].children);
+    posts = Array.from(document.querySelectorAll("#post-list")[0].children);
     main = document.querySelectorAll("main")[0];
     body = document.querySelectorAll("body")[0];
     footer = body.children[2];
 
     initializeNavBar(nav_bar_links);    
 
-    posts.forEach(post => {
-        post.addEventListener("click", (event) => {
-            let e = event.target;
-            let toShow = post.querySelector(".post-opened")
-            toShow.style.display = "block";
-            let cardText = post.querySelector(".post-preview").children[1].children[0];
-            // console.log(cardText.textOverflow);
-            // cardText.overflow = "visible";
-            // cardText.whiteSpace = "normal";
-            // cardText.textOverflow = "none";
-            posts.forEach(element => {
-                // let elementPreview = element.querySelector(".post-preview");
-                // let elementTitle = element.querySelector(".card-title");
-                // let elementText = element.querySelector(".card-text");
-                // let elementContainer = element.querySelector(".post-container");
-                if (element !== event.currentTarget) {
-                    element.style.display = "none";
-                }
-            });
+    posts.forEach(post_li => {
+        let post_preview = post_li.children[0].children[0];
+        post_preview.addEventListener("click", event => {
+            let post_preview_clicked = get_post_preview_from_click(event.target);
+
+            if (post_preview_clicked.parentNode.parentNode.className.includes("post-opened")) {
+                if (click_to_be_ignored(event.target)) return;
+                close_post();
+            } else {
+                open_post(post_preview_clicked.parentNode.parentNode);
+            }
         }, false);
     });
 
     body.addEventListener("click", (event) => {
         let e = event.target;
         if (main.children[0].contains(e) || footer.contains(e)) return;
-        posts.forEach(element => {
-            let toHide = element.querySelector(".post-opened");
-            //let cardText = post.querySelector(".post-preview").children[1].children[0];
-            // cardText.overflow = "hidden";
-            // cardText.textoverflow = "ellipsis";
-            element.style.display = "block";
-            toHide.style.display = "none";
-        });
+        close_post();
     });
 
 }, false);
+
+function open_post(post) {
+    close_posts_preview();
+    post.className = "list-element post-opened";
+}
+
+function close_post() {
+    posts.forEach(element => {
+        element.className = "list-element";
+    });
+}
+
+function close_posts_preview() {
+    posts.forEach(element => {
+        element.className = "list-element post-closed";
+    });
+}
+
+function get_post_preview_from_click(target) {
+    while (target.className !== "post-preview") {
+        target = target.parentNode;
+    }
+    return target;
+}
+
+function click_to_be_ignored(target) {
+    if (target.className.includes("profile-pic") ||
+        target.className.includes("tag-badge") ||
+        target.className.includes("username") ||
+        target.className.includes("like-button")) return true;
+}
 
