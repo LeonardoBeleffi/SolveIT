62274f102c87483222a83f5ac6ac1828bf4bb889
unknown <tiziano.vuksan@studio.unibo.it>
2023-08-27 17:25:33 +0200

    Fixed section nesting and other adjustments

--- src/template/search/search-main.php
+++ src/template/search/search-main.php
@@ -1,11 +1,12 @@
 <section>
+    <h1>Search people and tags</h1>
     <div class="container">
         <div class="search-section">
-            <input oninput="search(event)" type="text" class="form-input" placeholder="Search users or tags">
+            <input oninput="search(event)" type="text" class="form-input" placeholder="Search users or tags" title="Search users or tags">
             <div class="suggestions">
                 <!-- Suggestions will be displayed here -->
             </div>
         </div>
-    <?php require "./template/home/home-main.php" ?>
+    <?php require "./template/home/home-post-list.php" ?>
     </div>
 </section>
