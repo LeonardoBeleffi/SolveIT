3a342749565d0880b638a2350e976620b1a31065
Leonardo Beleffi <leonardo.beleffi@studio.unibo.it>
2023-08-29 13:19:55 +0200

    Add favicon

--- src/template/base.php
+++ src/template/base.php
@@ -12,6 +12,7 @@
         <meta name="keywords" content="Solveit, Solvit, Solveet, Slovit, Solveti, Bug, Solution, Solve, Fix, StackOverflow" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title><?php echo getTitle() ?></title>
+        <link rel="icon" type="image/x-icon" href="../resources/icons/logo-while.svg" />
         <!-- load CSS -->
         <?php loadRemoteCSS()?>
         <?php loadCSS()?>
