eb10b38e75fcb692c2f69e00d7d4548ec0f80174
unknown <tiziano.vuksan@studio.unibo.it>
2023-08-30 01:22:29 +0200

    Added about page anche changed login and register headers

---
+++ src/template/about/about-main.php
@@ -0,0 +1,9 @@
+<section>
+    <h1>About page</h1>
+    <p>Questo è un progetto realizzato a scopo didattico. Sviluppato da:</p>
+    <ul>
+        <li> Marco Sangiorgi </li>
+        <li> Leonardo Beleffi </li>
+        <li> Tiziano Vuksan</li>
+    <ul>
+</section>
