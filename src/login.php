0c8b87def0d7004ec7b67a0cf872c285eb096229
Leonardo Beleffi <leonardo.beleffi@studio.unibo.it>
2023-08-30 11:17:10 +0200

    Fix js error in login page

--- src/login.php
+++ src/login.php
@@ -15,7 +15,7 @@
     setFooter("login/login-footer.php");
     setCSS(["./css/login.css"]);
     setRemoteCSS(["https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"]);
-    setJS(["./js/login.js","./js/shared.js"]);
+    setJS(["./js/login.js"]);
 
     // require template
     require "template/base.php";
