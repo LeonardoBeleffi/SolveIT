aa37a3820e41ad86d3118102695c30887ac659e1
unknown <tiziano.vuksan@studio.unibo.it>
2023-08-30 02:18:22 +0200

    CSS and HTML adjustments

--- src/template/search/search-main.php
+++ src/template/search/search-main.php
@@ -2,7 +2,7 @@
     <h1>Search people and tags</h1>
     <div class="container">
         <div class="search-section">
-            <input oninput="search(event)" type="text" class="form-input" placeholder="Search users or tags" title="Search users or tags">
+            <input oninput="search(event)" type="search" class="form-input" placeholder="Search users or tags" title="Search users or tags">
             <div class="suggestions">
                 <!-- Suggestions will be displayed here -->
             </div>
